fdd48da673c95ac9eed0f90510461b2956b23858
seokenny <kenn.seo94@gmail.com>
2019-10-24 18:51:50 -0400

    Updates

---
+++ precache-manifest.1d330e5132baf20e805e725e4690c852.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c94ed2f7e9ae3c64af0f7f00da29c3",
+    "url": "/adoption/index.html"
+  },
+  {
+    "revision": "a1bb1afa06b18ec88405",
+    "url": "/adoption/static/js/main.1156edcf.chunk.js"
+  },
+  {
+    "revision": "360e24992ee8ba38cf5f",
+    "url": "/adoption/static/js/runtime-main.d712586e.js"
+  }
+]);
---
+++ static/js/main.1156edcf.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpadoption=this.webpackJsonpadoption||[]).push([[0],[function(o,n,p){o.exports=p(1)},function(o,n){}],[[0,1]]]);
+//# sourceMappingURL=main.1156edcf.chunk.js.map
